4d8c70df41c684563e158f9a4c292f6a2f958d71
borec8 <136652772+borec8@users.noreply.github.com>
2025-04-02 11:41:05 +0200

    Update open-andclose-menu.service.ts

--- WebFrontend/Toku_frontend/src/app/services/open-andclose-menu.service.ts
+++ WebFrontend/Toku_frontend/src/app/services/open-andclose-menu.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, delay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OpenAndcloseMenuService {
 
-  constructor() { }
+  // RxJS
+  private _visible: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+  public visible$: Observable<boolean> = this._visible.asObservable();
+  public dropdown$: Observable<boolean> = this._visible.asObservable().pipe(
+    delay(400)
+  );
+
+
+
+
+  showDropdown: boolean = false;
+  isVisible: boolean = false;
+
+  public constructor() { }
+
+  public toggleDropdownMenu() {
+    this._visible.next(!this._visible.value);
+    //this.isVisible = !this.isVisible 
+    //setTimeout( () => this.showDropdown = !this.showDropdown, 400)
+}
 }
